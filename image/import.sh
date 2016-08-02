7559a6fbaf0a01c261eabc7e1bd08e7d5f6c8ece
bwnyasse <risbonyasse@gmail.com>
2016-08-02 22:56:02 +0200

    Add bash script for import process

---
+++ image/import.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# @description
+# Script used to perform mongoimport
+# mongoimport is the native mongo tool for import ( https://docs.mongodb.com/manual/reference/program/mongoimport/ )
+#
+# @author bwnyasse
+##
+
+set -e
+
+echo "Job Import started: $(date)"
+
+mongoimport --quiet --host $MONGO_HOST:$MONGO_PORT --db $MONGO_DB_NAME --collection $MONGO_COLLECTION_NAME --type json --drop --file import.json --jsonArray
+
+echo "Job Import finished: $(date)"
