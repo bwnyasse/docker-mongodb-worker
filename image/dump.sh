1ff72ee69e93c4f1ce2ad1b9a4e19a84b413d347
bwnyasse <risbonyasse@gmail.com>
2016-08-10 22:43:38 +0200

    add dump mechanism

---
+++ image/dump.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# @description
+# Script used to perform mongodump
+# mongodump is the native mongo tool for dump ( https://docs.mongodb.com/manual/reference/program/mongodump/ )
+#
+# @author bwnyasse
+##
+
+set -e
+
+echo "Job Dump started: $(date)"
+
+DATE=$(date +%Y%m%d_%H%M%S)
+FILE="/backup/$MONGO_BACKUP_FILENAME-$DATE.tar.gz"
+OUTPUT="dump/"
+
+mongoexport --quiet --host $MONGO_HOST:$MONGO_PORT --out $OUTPUT
+tar -zcvf $FILE $OUTPUT
+rm -rf $OUTPUT
+
+echo "Job Dump finished: $(date)"
